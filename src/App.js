1a32ef21ecbe2c111143fcd8760bad00bd9050e1
Kevin Demetro <albinodemetrok@gmail.com>
2025-04-21 10:40:18 -0300

    criando empty cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import initialData from "./data.json";
 
+const initialCart = [
+  { name: "Waffle with Berries", price: 6.5, quantity: 4 },
+  { name: "Red Velvet Cake", price: 4.5, quantity: 1 },
+  { name: "Vanilla Bean Crème Brûlée", price: 7.0, quantity: 2 },
+];
+
 function App() {
   return (
     <div className="App">
-      <DessertsSection></DessertsSection>
+      <DessertsSection />
+      <Cart />
     </div>
   );
 }
@@ -44,6 +51,26 @@ function DessertCard({ dessert }) {
         <h6>{dessert.name}</h6>
         <span>{`R$${dessert.price.toFixed(2)}`}</span>
       </div>
+    </div>
+  );
+}
+
+function Cart() {
+  const isCartEmpty = true;
+  return (
+    <div className="cart">
+      <h4>Your Cart(X)</h4>
+      {isCartEmpty ? (
+        <div className="empty-cart">
+          <img
+            src="images/illustration-empty-cart.svg"
+            alt="illustration-empty-cart"
+          />
+          <p>Your added items will appear here</p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
