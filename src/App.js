02cdd12266cf91f39add376b1115dcfcb68f2cc2
Kevin Demetro <albinodemetrok@gmail.com>
2025-04-21 09:17:51 -0300

    implementando lista estática de desserts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import initialData from "./data.json";
+
 function App() {
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <DessertsSection></DessertsSection>
     </div>
   );
 }
 
+function DessertsSection() {
+  return (
+    <div>
+      <h2>Desserts</h2>
+      <DessertsGrid />
+    </div>
+  );
+}
+
+function DessertsGrid() {
+  const desserts = initialData;
+
+  return (
+    <ul className="desserts-grid">
+      {desserts.map((dessert) => (
+        <li>
+          <DessertCard dessert={dessert} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function DessertCard({ dessert }) {
+  return (
+    <div className="dessert-card">
+      <img src={dessert.image.desktop} alt={dessert.name} />
+      <div className="dessert-infos">
+        <p>{dessert.category}</p>
+        <h6>{dessert.name}</h6>
+        <span>{`R$${dessert.price.toFixed(2)}`}</span>
+      </div>
+      <button className="add-to-cart-btn">TESTE</button>
+    </div>
+  );
+}
 export default App;
